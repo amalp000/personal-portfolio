cfa00aac1e0f31503cab2f37bdb1c8da0572a4d6
amal p <amalp929@gmail.com>
2023-11-01 15:42:27 +0530

    professional skills styled and animation created

--- script.js
+++ script.js
@@ -39,3 +39,26 @@ let changeText=()=>{
 
 changeText();
 setInterval(changeText,3000)
+
+
+// circleskill.........................................
+
+
+const circles = document.querySelectorAll('.circle');
+circles.forEach(elem => {
+    var dots = elem.getAttribute("data-dots");
+    var marked = elem.getAttribute("data-precent");
+    var precent = Math.floor((dots * marked) / 100);
+    var points = "";
+    var rotate = 360 / dots;
+
+    for (let i = 0; i < dots; i++) {
+        points +=`<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+    }
+    elem.innerHTML = points;
+
+    const pointsMarked = elem.querySelectorAll('.points');
+    for (let i = 0; i < precent; i++) {
+        pointsMarked[i].classList.add('marked');
+    }
+});
