f58f8a6d49a00174af0ee6b4097ed09ca2c02db5
amal p <amalp929@gmail.com>
2023-11-05 12:17:23 +0530

    parallax added webpage resizing

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ words[currentWordIndex].style.opacity = "1";
 
 let changeText=()=>{
     let currentWord = words[currentWordIndex];
-    let nextWord = currentWordIndex === maxWordIndex ? words(0):words[currentWordIndex + 1];
+    let nextWord = currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
 
     Array.from(currentWord.children).forEach((letter,i)=>{
         setTimeout(()=>{
@@ -75,13 +75,13 @@ var mixer = mixitup('.portfolio-galery');
 
 
 let menuLi = document.querySelectorAll('header ul li a');
-let section =querySelectorAll('section');
+let section =document.querySelectorAll('section');
 
 
 function activeMenu(){
     let len =section.length;
-    while(--len &&window.scrollY + 97 < section[len].offsetTop){}
-    menuLi.forEach(sec=>sec.classList.remove("active"));
+    while(--len && window.scrollY + 97 < section[len].offsetTop){}
+    menuLi.forEach(sec => sec.classList.remove("active"));
     menuLi[len].classList.add("active");
 }
 
@@ -90,8 +90,54 @@ activeMenu();
 window.addEventListener("scroll",activeMenu);
 
 
+
+
 // // sticky navbar.........
- const header=document.querySelector("header");
+ const header = document.querySelector("header");
  window.addEventListener("scroll",function(){
     header.classList.toggle("sticky",window.scrollY > 50)
  })
+
+
+
+
+
+ // // togle icon navbar.........
+document.addEventListener("DOMContentLoaded",()=>{
+    let menuIcon = document.querySelector("#menu-icon");
+    let navlist = document.querySelector(".navlist");
+
+    menuIcon.onclick = ()=>{
+        menuIcon.classList.toggle("bx-x");
+        navlist.classList.toggle("open");
+    }
+
+
+    window.onscroll = ()=>{
+        menuIcon.classList.remove("bx-x");
+        navlist.classList.remove("open");
+    }
+
+
+});
+
+// parallax....................................
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show-items");
+        }else{
+            entry.target.classList.remove("show-items");
+        }
+    });
+});
+
+const scrollScale = document.querySelectorAll(".scroll-scale");
+scrollScale.forEach((el)=>observer.observe(el));
+
+const scrollBottom=document.querySelectorAll(".scroll-bottom");
+scrollBottom.forEach((el)=>observer.observe(el));
+
+const scrollTop=document.querySelectorAll(".scroll-top");
+scrollTop.forEach((el)=>observer.observe(el));
